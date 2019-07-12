c7ec009f8473e701e05177fa409a4a067bc9d8ba
brandon celeita <bceleita7@outlook.com>
2019-07-12 08:56:17 -0500

    primer despliegue

---
+++ src/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    database: {
+        host: 'byyum5tyi7mryclpwoh8-mysql.services.clever-cloud.com',
+        user: 'ummetdxhn7luqcbo',
+        password: 'L4ZSB92TzeqaIOq9N0dX',
+        database: 'byyum5tyi7mryclpwoh8'
+    }
+
+}
